65f726ed8a0ee7e6e7688e2323590f99f24f4447
FLCL <Dombovoz@gmail.com>
2025-05-15 13:42:33 +0300

    Не работают new delete edit/ сделал me api аналог аддатрибьют

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RESTAdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RESTAdminController.java
@@ -2,6 +2,7 @@ package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
 import ru.kata.spring.boot_security.demo.services.RoleService;
@@ -28,16 +29,16 @@ public class RESTAdminController {
         return ResponseEntity.ok(userService.index());
     }
     @PostMapping("/new")
-    public ResponseEntity<String> save(User user) {
+    public ResponseEntity<String> save(@RequestBody  User user) {
         userService.save(user);
         return ResponseEntity.ok("Пользователь готовченко");
     }
-    @PostMapping("/update")
-    public ResponseEntity<String> update(@RequestParam("id") Long id, User user) {
+    @PutMapping("/update")
+    public ResponseEntity<String> update(@RequestParam("id") Long id,@RequestBody User user) {
         userService.update(id, user);
         return ResponseEntity.ok("Обновлён чётко");
     }
-    @PostMapping("/delete")
+    @DeleteMapping("/delete")
     public ResponseEntity<String> delete(@RequestParam("id") Long id) {
         userService.delete(id);
         return ResponseEntity.ok("Удалён чётко");
@@ -49,5 +50,10 @@ public class RESTAdminController {
     @GetMapping("/users/{id}")
     public ResponseEntity<User> getOneUser(@PathVariable("id") Long id) {
         return ResponseEntity.ok(userService.show(id));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> getCurrentUser(@AuthenticationPrincipal User user) {
+        return ResponseEntity.ok(user);
     }
 }
--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RESTUserController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RESTUserController.java
@@ -1,23 +1,17 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.model.User;
-import ru.kata.spring.boot_security.demo.services.UserService;
+
 
 @RestController
 @RequestMapping("/api/user")
 public class RESTUserController {
 
-    private final UserService userService;
-
-    @Autowired
-    public RESTUserController(UserService userService) {
-        this.userService = userService;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<?> showUser(@PathVariable("id") Long id, Authentication authentication) {
